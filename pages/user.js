9b8816fe531b7729af9f0f75def249f117f151c3
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-08 20:00:35 +0600

    kd

--- pages/user.js
+++ pages/user.js
@@ -1,13 +1,15 @@
-function user({users}) {
+function user({ users }) {
     console.log(users);
-    
-    
-    
-    
-    
     return (
         <div>
             <h1>This is the user list.</h1>
+            {
+                users?.map(user => <div key={user.id}>
+                    <p> {user.name}</p>
+                    <p>{user.email}</p>
+                </div>)
+            }
+
         </div>
     );
 }
@@ -16,7 +18,6 @@ export async function getStaticProps() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
     console.log(data);
-
     return {
         props: {
             users: data
